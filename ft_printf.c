6a8197d8c57ff199d7086941fe6a595892181056
sandy <sandeep.kaur@gmx.com>
2022-11-09 21:02:24 +0100

    nov 9 21:02 updated

--- ft_printf.c
+++ ft_printf.c
@@ -12,42 +12,55 @@
 
 #include<stdarg.h>
 
-int ft_printf(const char *, ...)
+void	ft_flags(const char *str_char, va_list args, unsigned int *counter)
 {
-	va_list	ap;
-	int	i;
-	int	j;
+	int	result_c;
 
+	if (*str_char == 'c')
+	{
+		result_c = va_arg(args, int);
+		*counter += write(1, &result_c, 1);
+	}
+	else if (*str_char == 'u')
+		ft_putunbr(va_arg(args, unsigned int), counter);
+	else if (*str_char == 'd' || *str_char == 'i')
+		ft_putnbr(va_arg(args, int), counter);
+	else if (*str_char == 's')
+		ft_putstr(va_arg(args, const char *), counter);
+	else if (*str_char == 'x' || *str_char == 'X')
+		ft_puthex(va_arg(args, unsigned int), *str_char, counter);
+	else if (*str_char == 'p')
+	{
+		ft_putstr("0x", counter);
+		ft_puthexp(va_arg(args, unsigned long), counter);
+	}
+}
+
+int	ft_printf(const char *, ...)
+{
+	va_list			args;
+	unsigned int	i;
+	unsigned int	result;
+
+	if (!str)
+		return (-1);
 	i = 0;
-	i = 0;
-	va_start(ap, str);
-	while (str && str[i])
+	result = 0;
+	va_start(args, str);
+	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
 		{
-			if (str[i + 1] == 'c')
-			{
-				va_arg(ap, int);
-			}
-			else if (str[i + 1] == 's')
-			{
-				
-			}
-			else if (str[i + 1] == 'p')
-			{
-			}
-			else if (str[i + i] == 'd' || str[i + i] == 'u' || str[i + i] == 'u')
-			{
-			}
-			else if (str[i + 1] == 'x' || str[i + i] == 'X')
-			{
-
-			}
+			if (str[i + 1] == '%')
+				result += write(1, "%", 1);
 			else
-			{
-
-			}
+				ft_flags(&str[i + 1], args, &result);
+			i++;
 		}
+		else
+			result += write(1, &str[i], 1);
+		i++;
 	}
-	va_end(ap);
+	va_end(args);
+	return (result);
 }
